9ac9e70f408e0f654774210b469a68bc4d27d963
Ines <ines.chelly@student.evbg.de>
2025-05-30 09:40:47 +0200

    sgsg

--- JUMANJI.java
+++ JUMANJI.java
@@ -1,33 +1,11 @@
 
-/**
- * Beschreiben Sie hier die Klasse JUMANJI.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
 public class JUMANJI
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse JUMANJI
-     */
+    
     public JUMANJI()
     {
-        // Instanzvariable initialisieren
-        x = 0;
+  
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+
 }
