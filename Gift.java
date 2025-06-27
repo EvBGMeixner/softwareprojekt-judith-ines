959ed7fe11eb41c014e186e7d808a48399f400f8
Judith <judith.pauli@student.evbg.de>
2025-06-27 10:44:38 +0200

    dsfs

---
+++ Gift.java
@@ -0,0 +1,24 @@
+
+public class Gift extends FIGUR
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    public Gift()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
