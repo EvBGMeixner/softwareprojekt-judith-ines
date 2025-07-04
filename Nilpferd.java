e6e0ca07f8b13ab97f7f551935dde3df1bc7e0dd
Ines <ines.chelly@student.evbg.de>
2025-07-04 10:06:36 +0200

    zio

--- Nilpferd.java
+++ Nilpferd.java
@@ -1,17 +1,10 @@
 
 public class Nilpferd extends FIGUR
 {
-<<<<<<< HEAD
-=======
-    
->>>>>>> cb11a4eb49fcdf4547299f0eed4008d6302ebd79
+
     public Nilpferd()
     {
        
     }
-<<<<<<< HEAD
-=======
 
-    
->>>>>>> cb11a4eb49fcdf4547299f0eed4008d6302ebd79
 }
