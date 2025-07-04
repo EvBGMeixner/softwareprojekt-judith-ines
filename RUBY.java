cb11a4eb49fcdf4547299f0eed4008d6302ebd79
Ines <ines.chelly@student.evbg.de>
2025-07-04 09:30:18 +0200

    gkuk

--- RUBY.java
+++ RUBY.java
@@ -1,14 +1,41 @@
 
 public class RUBY extends FIGUR
 {
-
+    double deltaX;
+    double deltaY;
     public RUBY()
     {
-       
+        super("");
+        deltaX = 0;
+        deltaY = 0;
+
+    }
+    public void bewegenR(){
+        verschiebenUm(deltaX, 0);
     }
 
-    public int beispielMethode(int y)
-    {
-        
+    public void bewegeNachLinksR(){
+        deltaX = -0.2;
+    }
+
+    public void bewegeNachRechtsR(){
+        deltaX = 0.2;
+    }
+
+    public void anhalten(){
+        deltaX = 0;
+    }
+
+    public void springen(){
+        for(int i=4; i>-4; i--)
+        {deltaY = i*0.1;
+            long l=(long) 0.1875;
+            try {
+                wait(l);
+            }
+            catch(Exception e) {
+                //  Block of code to handle errors
+            }
+        }
     }
 }
