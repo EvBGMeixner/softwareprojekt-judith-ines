d89f665adc75fd77974471828b79e0d6d4e45788
Ines <ines.chelly@student.evbg.de>
2025-07-05 11:15:03 +0200

    dsd

--- Liane.java
+++ Liane.java
@@ -9,22 +9,5 @@ public class Liane extends FIGUR
     void lianeziehen(){
         
     }
-    if(taste == 38){
-            Ruby.deltaX=0;
-            Ruby.deltaY=0
-            Franklin.deltaX=0;
-            Franklin.deltaY=0;
-            pausiert = true;
-            anzeigeLeben.setzeInhalt("Leben:"+leben+ "                                        PAUSE");            
-        }if (taste == 40){
-            pausiert = false;
-            anzeigeLeben.setzeInhalt("Leben:"+leben);
-        }
-        if(pausiert == false){
-            if(taste == 37){
-                schlaeger.bewegeNachLinks();
-            }else if(taste == 39){
-                schlaeger.bewegeNachRechts();
-            }
-        }
+    
 }
